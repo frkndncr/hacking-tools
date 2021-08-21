b192045e469f82907df58f575be2d318026bbf6a
B3zkurt <68972804+B3zkurt@users.noreply.github.com>
2021-08-21 14:48:07 +0300

    Add files via upload

---
+++ b3zkurt-hacking-tools.py
@@ -0,0 +1,186 @@
+import os
+import webbrowser
+import time
+
+os.system("clear")
+print("#####################################")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("             HOŞGELDİN!              ")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("#####################################")
+
+time.sleep(1)
+os.system("clear")
+print("#####################################")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("        B3ZKURT HACKİNG TOOLS        ")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("#####################################")
+
+print("")
+print("")
+print("1.Shell Scanner")
+print("2.Admin Panel Finder")
+print("3.Sqlmap")
+print("4.Nmap ( Kali Linux ) ")
+print("5.Metasploit ( Kali Linux )")
+print("6.Cadaver ( Kali Linux ) ")
+print("7.Hydra ( Kali Linux ) ")
+print("8.Nikto ( Kali Linux ) ")
+print("9.Wpscan ( Kali Linux ) ")
+print("10.Hashcat ( Kali Linux ) ")
+print("11.Md5 Şifre Kırma")
+print("12.Burpsuite ( Kali Linux )")
+print("13.Hackbar İndirme ( Fire Fox + Hackbar ) ")
+print("14.Shell Arşivi")
+print("15.Ddos")
+print("16.ReconCobra")
+print("17.KillShot")
+print("18.Commix")
+print("19.Waes")
+print("20.Hacking Tools Arşivi")
+print("21.İnstagram Brute Force Araçları")
+print("22.Facebook Brute Force Araçları")
+print("23.İnstagram Flood Tool'u")
+print("24.Phishing")
+print("25.Twitter Brute Force Araçları")
+print("26.Wordpress Hacking Set'i")
+print("27.3k Password List")
+print("28.Sql Dorklar")
+print("29.Google Hacking Dorklar")
+print("30.Maltego ( Kali Linux )")
+print("31.Beef")
+print("32.Netsniff")
+print("33.ESpoofing")
+print("34.Netcat")
+print("")
+print("")
+
+islem = input("İşlem Numarası Giriniz: ")
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+if islem =="1":
+    os.system("git clone https://github.com/b3zkurt/shell-scanner")
+    webbrowser.get(chrome_path).open("https://github.com/b3zkurt/shell-scanner")
+
+if islem =="2":
+    os.system("git clone https://github.com/hawk-unity/hawkadmin")
+    webbrowser.get(chrome_path).open("https://github.com/hawk-unity/hawkadmin")
+
+if islem =="3":
+    webbrowser.get(chrome_path).open("https://www.youtube.com/watch?v=eqZztfYMaTg")
+
+if islem =="4":
+    os.system("nmap")
+    
+if islem =="5":
+    os.system("msfconsole")
+
+if islem =="6":
+    os.system("cadaver")
+
+if islem =="7":
+    os.system("hydra")
+
+if islem =="8":
+    os.system("nikto")
+
+if islem =="9":
+    os.system("wpscan --help")
+
+if islem =="10": 
+    os.system("hashcat")
+
+if islem =="11": 
+    webbrowser.get(chrome_path).open("https://github.com/sybercodev1real/minimd5")
+
+
+if islem =="12": 
+    os.system("burpsuite")
+
+if islem =="13": 
+    webbrowser.get(chrome_path).open("https://www.youtube.com/watch?v=sNsbOlKoKx8")
+    webbrowser.get(chrome_path).open("http://www.mediafire.com/file/5y7pw6azw1n0oy4/Firefox_Setup_52.6.0.exe/file")
+    webbrowser.get(chrome_path).open("https://www.mediafire.com/file/ia0nd005epco4an/HackBar_All_In_One_Firefox.zip/file")
+
+if islem =="14":
+    webbrowser.get(chrome_path).open("https://disk.yandex.com.tr/d/fuQAcHf4lCVtuA")
+
+if islem =="15":
+    webbrowser.get(chrome_path).open("git clone https://github.com/cyweb/hammer")
+    webbrowser.get(chrome_path).open("git clone https://github.com/gkbrk/slowloris")
+    webbrowser.get(chrome_path).open("git clone https://github.com/grafov/hulk")
+
+if islem =="16":
+    webbrowser.get(chrome_path).open("https://github.com/Agent00049/ReconCobra")
+
+if islem =="17": 
+    webbrowser.get(chrome_path).open("https://github.com/bahaabdelwahed/killshot")
+
+if islem =="18":
+    webbrowser.get(chrome_path).open("https://github.com/commixproject/commix")
+
+if islem =="19":
+    webbrowser.get(chrome_path).open("https://github.com/Shiva108/WAES")
+
+if islem =="20":
+    webbrowser.get(chrome_path).open("https://disk.yandex.com.tr/d/OI47dOESP_i1QA")
+
+if islem =="21": 
+    webbrowser.get(chrome_path).open("https://github.com/0xfff0800/Brute-force-Instagram-2021")
+    webbrowser.get(chrome_path).open("https://github.com/OgulcanKacarr/InstaBrute")
+    webbrowser.get(chrome_path).open("https://github.com/Bitwise-01/Instagram-")
+
+if islem =="22": 
+    webbrowser.get(chrome_path).open("https://github.com/IAmBlackHacker/Facebook-BruteForce")
+    webbrowser.get(chrome_path).open("https://github.com/Oseid/FaceBoom")
+    webbrowser.get(chrome_path).open("https://github.com/m-primo/Facebook-Brute-Force")
+
+if islem =="23": 
+    webbrowser.get(chrome_path).open("https://github.com/B3zkurt/instagram-flood")
+
+if islem =="24": 
+    webbrowser.get(chrome_path).open("https://github.com/An0nUD4Y/blackeye")
+    webbrowser.get(chrome_path).open("https://github.com/suljot/shellphish")
+    webbrowser.get(chrome_path).open("https://github.com/DarkSecDevelopers/HiddenEye-Legacy")
+
+if islem =="25":
+    webbrowser.get(chrome_path).open("https://github.com/0xfff0800/Brute-Forc-Twitter-")
+    webbrowser.get(chrome_path).open("https://github.com/Oseid/TWTBOOM")
+
+if islem =="26": 
+    webbrowser.get(chrome_path).open("https://www.youtube.com/watch?v=8uxBLba_f8Q&t=11s")
+    webbrowser.get(chrome_path).open("https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbnNjMm9ZZ1ROOWxIdmJKYVhfNXQtaFpKaEZQQXxBQ3Jtc0trRzNGQUhQdGxXd0hrd3dYTkhiZktzMG9UVEhQV1B4TDFTdk8xOHFJWmVVNXlYRGxJNy1FbmFoTEw1cWdnOEZaSEUtaWRzZTRvWWV3WlQtd1JaRW9fdzFNbkMxU29NTThRY1MwTTU2MzVTT0Z6Sl9UQQ&q=https%3A%2F%2Fwww.dosya.tc%2Fserver34%2Fybvg5h%2Fbf.rar.html")
+
+if islem =="27":
+    webbrowser.get(chrome_path).open("https://github.com/B3zkurt/ftp/blob/main/pass.txt")
+
+if islem =="28":
+    webbrowser.get(chrome_path).open("https://gist.github.com/m0k1/ada77aacefe3dcae7bc2")
+
+if islem =="29":
+    webbrowser.get(chrome_path).open("https://www.boxpiper.com/posts/google-dork-list")
+
+if islem =="30":
+    os.system("maltego")
+
+if islem =="31":
+    webbrowser.get(chrome_path).open("https://github.com/beefproject/beef")
+
+if islem =="32":
+    webbrowser.get(chrome_path).open("https://github.com/netsniff-ng/netsniff-ng")
+
+if islem =="33":
+    webbrowser.get(chrome_path).open("https://github.com/mo-xiaoxi/ESpoofing")
+
+if islem =="34":
+    webbrowser.get(chrome_path).open("https://github.com/diegocr/netcat")
